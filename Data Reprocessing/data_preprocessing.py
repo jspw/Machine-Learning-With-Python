3e3cbf1cfb1ff3e163d01666737d56e3873ac063
mh-shifat <mhshifat757@gmail.com>
2020-01-27 05:45:27 +0600

    import libraries

---
+++ Data Reprocessing/data_preprocessing.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 05:30:46 2020
+
+@author: JackSparrow
+"""
+
+# importing libraries
+
+import numpy as np  #for mathematics
+import matplotlib.pyplot as plt #for plot
+import pandas as pd #to import dataset and manage dataset
+
