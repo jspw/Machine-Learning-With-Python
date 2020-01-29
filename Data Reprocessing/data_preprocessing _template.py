a7436f015dac869ae4f40e930da2b879818459fa
mh-shifat <mhshifat757@gmail.com>
2020-01-29 06:38:25 +0600

    Data Preprocessing done
    import libraries
    check active
    create dataset
    deal with missing datas
    catagorical data encoded
    split data into training set and test set
    scalling data into a range

--- Data Reprocessing/data_preprocessing _template.py
+++ Data Reprocessing/data_preprocessing _template.py
@@ -19,6 +19,7 @@ dataset = pd.read_csv("Data.csv")
 X = dataset.iloc[:, :-1].values #take all collum except the last one thats wht -1
 Y = dataset.iloc[:,3].values #take the 3no colum/purchase colum (index start with 0)
 
+"""
 #taking care of missing data
 #from sklearn.preprocessing import Imputer #this shit is not working in dnt know why
 from sklearn.impute import SimpleImputer 
@@ -42,12 +43,14 @@ X = onehotencoder.fit_transform(X)
 labelencoder_Y  = LabelEncoder()
 Y = labelencoder_Y.fit_transform(Y)
 
+"""
+
 
 #spliting data for training set and test set
 from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test  = train_test_split(X,Y,test_size=0.2,random_state = 0)
 
-
+"""
 
 # scalling datas in a fixed range
 from sklearn.preprocessing import StandardScaler
@@ -55,7 +58,7 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
-
+"""
 
 
 
